ab3ac201bee627b40c92a9591c27f800f93db5c8
パクギョンソク <boardguy1024@gmail.com>
2023-09-09 23:58:06 +0900

    Initial Commit

---
+++ SlideOutMenu/SlideOutMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideOutMenuApp.swift
+//  SlideOutMenu
+//
+//  Created by パクギョンソク on 2023/09/09.
+//
+
+import SwiftUI
+
+@main
+struct SlideOutMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
